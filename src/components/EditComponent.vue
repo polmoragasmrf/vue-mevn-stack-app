8598bca3168027345b536d62a2d9f9ec6ebcd43a
polmoragasmrf <pol.moragas@marfeel.com>
2020-09-11 00:12:39 +0200

    add vue app

---
+++ src/components/EditComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            edit
+            <!-- Update Student content -->
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        }
+    }
+</script>
---
+++ src/components/ListComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            list
+            <!-- Display Student List -->
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        }
+    }
+</script>
